120043b8fc614ec0d79a0d367d4ad3e93c7b7873
Jon-Magne Aa. Jønholt <jon-magneaa.jonholt@Jon-Magnes-MacBook-Pro.local>
2019-12-05 13:18:06 +0100

    Updates

---
+++ precache-manifest.8a89031e74a2355452c5202eeee570cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c360df3fb3fc9e0e7994f60b273020c7",
+    "url": "/Tema_4-React/index.html"
+  },
+  {
+    "revision": "23d2dc4ee42093fdb552",
+    "url": "/Tema_4-React/static/css/main.91bf968d.chunk.css"
+  },
+  {
+    "revision": "586774eea3351b0c066b",
+    "url": "/Tema_4-React/static/js/2.2db8ca07.chunk.js"
+  },
+  {
+    "revision": "23d2dc4ee42093fdb552",
+    "url": "/Tema_4-React/static/js/main.caa449d5.chunk.js"
+  },
+  {
+    "revision": "f3d026ddade0e0aae568",
+    "url": "/Tema_4-React/static/js/runtime-main.f5d7c12f.js"
+  }
+]);
